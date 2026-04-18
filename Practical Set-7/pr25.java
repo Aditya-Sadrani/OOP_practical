8089decfef7716ec2e1284523c4381d6b2ac7413
Aditya-Sadrani <adityasadrani511@gmail.com>
2026-04-18 09:39:09 +0530

    Create pr25.java

---
+++ Practical Set-7/pr25.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Table {
+    synchronized void printTable(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+            try {
+                Thread.sleep(500);
+            } 
+            catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println();
+    }
+}
+
+class Thread1 extends Thread {
+    Table t;
+
+    Thread1(Table t) {
+        this.t = t;
+    }
+
+    public void run() {
+        t.printTable(5);
+    }
+}
+
+class Thread2 extends Thread {
+    Table t;
+
+    Thread2(Table t) {
+        this.t = t;
+    }
+
+    public void run() {
+        t.printTable(7);
+    }
+}
+
+public class pr25 {
+    public static void main(String[] args) {
+        Table t = new Table();
+
+        Thread1 t1 = new Thread1(t);
+        Thread2 t2 = new Thread2(t);
+
+        t1.start();
+        t2.start();
+    }
+}
