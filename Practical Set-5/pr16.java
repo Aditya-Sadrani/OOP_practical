fa33156fe4018dc5d9ff92cfaefd290fc416f988
Aditya-Sadrani <adityasadrani511@gmail.com>
2026-04-18 09:13:47 +0530

    Create pr16.java

---
+++ Practical Set-5/pr16.java
@@ -0,0 +1,29 @@
+import java.util.*:
+
+interface Classify {
+    String getDivision(double average);
+}
+
+class Result implements Classify {
+    public String getDivision(double average) {
+        if (average >= 60) {
+            return "First Division";
+        } 
+        else {
+            return "No Division";
+        }
+    }
+}
+
+public class pr16 {
+    public static void main(String[] args) {
+        Result r = new Result();
+
+        double average = 65.0;
+
+        String division = r.getDivision(average);
+
+        System.out.println("Average : " + average);
+        System.out.println("Division : " + division);
+    }
+}
