b5703f9a034ba347892f2e599f50cc67c453c3d2
Aditya-Sadrani <adityasadrani511@gmail.com>
2026-04-18 09:41:57 +0530

    Create pr29.java

---
+++ Practical Set-9/pr29.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class pr29 {
+    public static void main(String[] args) {
+        ArrayList<Integer> marks = new ArrayList<>();
+
+        marks.add(75);
+        marks.add(82);
+        marks.add(90);
+        marks.add(68);
+        marks.add(88);
+
+        System.out.println("Marks:");
+        for (int m : marks) {
+            System.out.println(m);
+        }
+
+        int highest = Collections.max(marks);
+        int lowest = Collections.min(marks);
+
+        System.out.println("\nHighest Marks : " + highest);
+        System.out.println("Lowest Marks : " + lowest);
+    }
+}
